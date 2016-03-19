5c12e99fff0b9b6284fbc98708e4ffdaab16b67b
unknown <mbilosha@gmail.com>
2016-03-19 13:16:15 +0530

    all

--- src/Events/Bundle/EventsBundle/Form/Type/UserType.php
+++ src/Events/Bundle/EventsBundle/Form/Type/UserType.php
@@ -58,7 +58,7 @@ class UserType extends AbstractType {
     }
 
     public function getDefaultOptions(array $options) {
-        return array('csrf_protection' => true);
+        return array('csrf_protection' => FALSE);
     }
 
     public function getName() {
