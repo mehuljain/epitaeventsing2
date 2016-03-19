0cd5697fd3094f2da6fb188d5e5aa61a3ad932f6
unknown <mbilosha@gmail.com>
2016-03-19 10:07:17 +0530

    Add

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -1,7 +1,7 @@
 $secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
 
 $container->setParameter('database_driver', 'pdo_mysql');
-$container->setParameter('database_host', $secrets['MYSQL']['HOST']);
-$container->setParameter('database_name', $secrets['MYSQL']['DATABASE']);
-$container->setParameter('database_user', $secrets['MYSQL']['USER']);
-$container->setParameter('database_password', $secrets['MYSQL']['PASSWORD']);
+$container->setParameter('database_host', 'epitaeventsing2.mysql.eu2.frbit.com');
+$container->setParameter('database_name', 'epitaeventsing2');
+$container->setParameter('database_user', 'epitaeventsing2');
+$container->setParameter('database_password', 'Le.fPnkIXtTOt3zeZ-TA+GwT');
