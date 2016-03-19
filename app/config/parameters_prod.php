c12f897414379b137e359eafbc8835760a1e322c
unknown <mbilosha@gmail.com>
2016-03-19 13:46:25 +0530

    all

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -1,4 +1,4 @@
-$secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
+<!--$secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);-->
 
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', 'epitaeventsing2.mysql.eu2.frbit.com');
